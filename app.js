27cfbbc84caadd09d967ce11ee4625ed9df265c8
Mattia Mion <mattia.mion@gmail.com>
2026-02-05 15:41:13 +0100

    previw in html usando il tag h1

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('hello mattia')
+app.get('/', (req, res) => { //prima rotta preincipale
+    res.send('<h1>Server del mio blog</h1>') //il type di default é 'html'
 })
 
 app.listen(port, () =>{
