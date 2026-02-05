ef94771765accec02553052194189cdd1573d58a
Mattia Mion <mattia.mion@gmail.com>
2026-02-05 15:55:44 +0100

    creo oggetto js da inviare nella res

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => { //prima rotta preincipale
-    res.send('<h1>Server del mio blog</h1>') //il type di default é 'html'
+
+    console.log(res);
+    
+    //creo un oggetto js da inviare nella res
+
+    const posts = {
+        title: "Ciambellone",
+        body: "il ciambellone é molto buono",
+        image: "inserisci url o path img",
+        tags: ["super", "top", "lapiubuona"]
+    }
+
+    res.json(posts)
+
+    //res.send('<h1>Server del mio blog</h1>') //il type di default é 'html'
 })
 
 app.listen(port, () =>{
